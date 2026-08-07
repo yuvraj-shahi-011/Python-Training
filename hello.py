2f445d5b4e91bcb80a93c3a9f70429a5e7552b16
yuvraj-shahi-011 <yuvrajshahi2081@gmail.com>
2026-08-07 13:28:52 +0530

    Initial commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+list1=[1, 2, 3, 4, 5]
+list1[4]=8
+even=0
+odd=0
+for i in list1:
+    if i % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+print("Number of even in the list:", even)
+print("Number of odd in the list:", odd)
+sum=0
+average=0
+for i in list1:
+    sum += i
+    average=sum / len(list1)
+print("average of the list:", average)
